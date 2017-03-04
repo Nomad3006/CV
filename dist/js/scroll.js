7b6fcc20b4ce09c42e89aa9b5954fd4c35a1c017
Nomad <palamara.thomas@gmail.com>
2017-03-04 12:51:26 +0100

    postbar

--- dist/js/scroll.js
+++ dist/js/scroll.js
@@ -8,11 +8,39 @@ $(document).ready(function () {
 		});
 	});
 });
+
+var nav = $(".masthead");
+var nav_static = "nav-scaled";
+var off_height = document.getElementById('profil').offsetTop;
+
+$(document).ready(function(){
+	$(document).scroll(function(){
+
+		if( $(this).scrollTop() > 1 ) {
+			nav.addClass(nav_static);
+		} else {
+			nav.removeClass(nav_static);
+		}
+	});
+});
+
+//function animations(element,addedclass){
+//	console.log(element);
 //$(document).ready(function() {
-//	$('a').on('click', function() { // Au clic sur un élément
-//		var page = $(this).attr('href'); // Page cible
-//		var speed = 750; // Durée de l'animation (en ms)
-//		$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-//		return false;
-//	});
+//    $(window).scroll( function(){
+//        $(element).each( function(i){
+//            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+//            var bottom_of_window = $(window).scrollTop() + $(window).height();
+//            if( bottom_of_window > bottom_of_object ){
+//                $(this).addClass(addedclass);
+//            }
+//        });
+//    });
 //});
+//}
+
+//animations('.hiden','fadeInLeft');
+//animations('._fadeIn','fadeIn');
+////animations('.circle','rotate')
+
+
