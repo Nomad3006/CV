88696aa1241838d4f40e4329027f9a54301befb3
Web Janv 2015 <Web Janv 2015>
2017-03-13 17:03:29 +0100

    transition

---
+++ dist/js/animations.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	var t = document.getElementsByClassName('decal_right')
+	var w = new Array();
+	for(var i=0;i<t.length;i++){
+		console.info(t[i].offsetWidth)
+		w[i] = 592-t[i].offsetWidth;
+		t[i].style.right = w[i]+"px";
+		console.info(t[i] +' = '+ t[i].style.right)
+	}
+	$(".decal_left").removeClass('decal_left');
+	$(".decal_right").removeClass('decal_right');
+	$(".decal_top").removeClass('decal_top');
+	$(".decal_bot").removeClass('decal_bot');
+
+
+	console.info('ok');
+});
--- dist/js/scroll.js
+++ dist/js/scroll.js
@@ -16,12 +16,45 @@ var off_height = document.getElementById('profil').offsetTop;
 $(document).ready(function(){
 	$(document).scroll(function(){
 
-		if( $(this).scrollTop() > 1 ) {
+		if( $(this).scrollTop() > 800 ){
 			nav.addClass(nav_static);
 		} else {
 			nav.removeClass(nav_static);
 		}
 	});
+});
+
+
+
+
+var aChildren = $("nav").children(); // find the a children of the list items
+var aArray = []; // create the empty aArray
+for (var i=0; i < aChildren.length; i++) {
+	var aChild = aChildren[i];
+	var ahref = $(aChild).attr('href');
+	aArray.push(ahref);
+}
+$(window).scroll(function(){
+	var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+	var windowHeight = $(window).height(); // get the height of the window
+	var docHeight = $(document).height();
+	for (var i=0; i < aArray.length; i++) {
+		var theID = aArray[i];
+		var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+		var divHeight = $(theID).height(); // get the height of the div in question
+		if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+			$("a[href='" + theID + "']").addClass("active");
+		} else {
+			$("a[href='" + theID + "']").removeClass("active");
+		}
+	}
+	if(windowPos + windowHeight == docHeight) {
+		if (!$("nav li:last-child a").hasClass("active")) {
+			var navActiveCurrent = $(".active").attr("href");
+			$("a[href='" + navActiveCurrent + "']").removeClass("active");
+			$("nav li:last-child a").addClass("active");
+		}
+	}
 });
 
 //function animations(element,addedclass){
