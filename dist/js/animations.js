bbfb52944b9b91b9901d518716d271fe969ecb1a
Web Janv 2015 <Web Janv 2015>
2017-03-14 11:30:52 +0100

    endcover

--- dist/js/animations.js
+++ dist/js/animations.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-	var t = document.getElementsByClassName('decal_right')
-	var w = new Array();
-	for(var i=0;i<t.length;i++){
-		console.info(t[i].offsetWidth)
-		w[i] = 592-t[i].offsetWidth;
-		t[i].style.right = w[i]+"px";
-		console.info(t[i] +' = '+ t[i].style.right)
-	}
+//	var t = document.getElementsByClassName('decal_right')
+//	var w = new Array();
+//	for(var i=0;i<t.length;i++){
+//		console.info(t[i].offsetWidth)
+//		w[i] = 592-t[i].offsetWidth;
+//		t[i].style.right = w[i]+"px";
+//		console.info(t[i] +' = '+ t[i].style.right)
+//	}
 	$(".decal_left").removeClass('decal_left');
 	$(".decal_right").removeClass('decal_right');
 	$(".decal_top").removeClass('decal_top');
 	$(".decal_bot").removeClass('decal_bot');
+	$(".fading").removeClass('fading');
 
 
-	console.info('ok');
+	console.info($(".fading"));
 });
